23a4c872cc57ab836fa2e79aa93782f8b67a7a94
cfbender <codyfbender@gmail.com>
2019-10-18 16:37:03 -0600

    Add basic auth0 functionality

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,21 @@
 import React from "react";
-import "./App.css";
+import NavBar from "./components/NavBar";
+import { useAuth0 } from "./react-auth0-spa";
 
-const App: React.FC = () => {
-  return <div className="App"></div>;
-};
+function App() {
+  const { loading } = useAuth0();
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="App">
+      <header>
+        <NavBar />
+      </header>
+    </div>
+  );
+}
 
 export default App;
