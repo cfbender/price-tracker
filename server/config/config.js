590485f358b38bec38d21961c991aede6b0b0af3
cfbender <codyfbender@gmail.com>
2019-10-24 19:52:08 -0600

    Fix test

--- server/config/config.js
+++ server/config/config.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   test: {
     username: "root",
-    password: "",
+    password: "sqlPass",
     database: "prices_test",
     host: "127.0.0.1",
     dialect: "mysql"
--- server/src/tests/database.test.ts
+++ server/src/tests/database.test.ts
@@ -1,4 +1,5 @@
 import http, { Server } from "http";
+import supertest from "supertest";
 import fetch from "node-fetch";
 import server from "../server";
 //@ts-ignore
@@ -8,18 +9,20 @@ describe("GET /api/examples", function() {
   // Before each test begins, create a new request server for testing
   // & delete all examples from the db
   let connection: Server;
+  let request: supertest.SuperTest<supertest.Test>;
   beforeAll(async function(done) {
     await db.sequelize.sync({ force: true });
     connection = http.createServer(server);
-    connection.listen(5000, done);
-    return;
+    connection.listen(done);
+    request = supertest(connection);
   });
 
   afterAll(done => {
     connection.close(done);
+    db.sequelize.close();
   });
 
-  it("should find all examples", async function() {
+  it("should find all examples", async () => {
     // Add some examples to the db to test with
     await db.Example.bulkCreate(
       [
@@ -29,28 +32,22 @@ describe("GET /api/examples", function() {
       { individualHooks: true }
     );
     // Request the route that returns all examples
-    const response = await fetch("http://localhost:5000/api/examples", {
-      method: "GET"
-    });
-
-    const json = await response.json();
-    const responseStatus = response.status;
-
+    const response = await request.get("/api/examples");
     // Run assertions on the response
 
-    expect(responseStatus).toEqual(200);
+    expect(response.status).toEqual(200);
 
-    expect(Array.isArray(json)).toBeTruthy();
-    expect(json).toHaveLength(2);
+    expect(Array.isArray(response.body)).toBeTruthy();
+    expect(response.body).toHaveLength(2);
 
-    expect(json[0]).toEqual(
+    expect(response.body[0]).toEqual(
       expect.objectContaining({
         text: "First Example",
         description: "First Description"
       })
     );
 
-    expect(json[1]).toEqual(
+    expect(response.body[1]).toEqual(
       expect.objectContaining({
         text: "Second Example",
         description: "Second Description"
